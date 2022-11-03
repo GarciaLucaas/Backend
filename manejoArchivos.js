ea3ae8e1d59da64a22142042e1311915ef664326
Lucasmartinez <garcia.lucaset36@gmail.com>
2022-11-03 20:05:20 -0300

    Multer & Router

--- manejoArchivos.js
+++ manejoArchivos.js
@@ -1,46 +1,60 @@
 const fs = require('fs');
-fs.writeFileSync("manejo1.txt")
+fs.writeFileSync("manejo1.js",[])
 const express = require ('express');
 const app = express();
-
+const {Router}= express;
+const router = new Router();
+let contadorId = 0
 class Contenedor{
-    // save() {
-    //     fs.readFile('manejo1.txt','utf-8',(error,contenido)=>{
-    //         if(error){throw new Error('Error')}
-    //         console.log(contenido);
-    //     })
-    // }
+    save = async(obj) => {
+        
+        contadorId = contadorId + 1
+        Object.defineProperty(obj, 'id', {
+            value:contadorId,
+            writable: true,
+            enumerable: true,
+            configurable: true
+        })
+
+        let leer = fs.readFileSync("./manejo1.js","utf-8")
+        let objetosLeidos = JSON.parse(leer)
+        objetosLeidos.push(obj)
+
+        
+        fs.writeFileSync("./manejo1.js",JSON.stringify(objetosLeidos))
+        console.log(contadorId)
+    }
     getById = async(id) =>{
-        let leer = fs.readFileSync("manejo1.txt","utf-8")
+        let leer = fs.readFileSync("manejo1.js","utf-8")
         let objetosLeidos = JSON.parse(leer)
         const result = objetosLeidos.filter(obj => obj.id == id);
         console.log(result[0])
     }
     getAll = async() =>{
-        let leer = fs.readFileSync("manejo1.txt","utf-8")
+        let leer = fs.readFileSync("manejo1.js","utf-8")
         let objetosLeidos = JSON.parse(leer)
         console.log(objetosLeidos)
     }
 
     deleteById =async(idEliminar) =>{
-        let leer = fs.readFileSync("manejo1.txt","utf-8")
+        let leer = fs.readFileSync("manejo1.js","utf-8")
         let objetosLeidos = JSON.parse(leer)
         let nuevaLista = objetosLeidos.filter((item) => item.id !== idEliminar)
-        fs.writeFileSync("manejo1.txt",JSON.stringify(nuevaLista))
+        fs.writeFileSync("manejo1.js",JSON.stringify(nuevaLista))
     }
 
     deleteAll = async()=>{
-        let leer = fs.readFileSync("manejo1.txt","utf-8")
+        let leer = fs.readFileSync("manejo1.js","utf-8")
         let objetosLeidos = JSON.parse(leer)
-        fs.writeFileSync("manejo1.txt",JSON.stringify([]))
+        fs.writeFileSync("manejo1.js",JSON.stringify([]))
     }
 }
 const producto = new Contenedor("Regla")
+//let leer = fs.readFileSync("manejo1.js","utf-8")
+//let objetosLeidos = JSON.parse(leer)
 
 
-let leer = fs.readFileSync("manejo1.txt","utf-8")
-let objetosLeidos = JSON.parse(leer)
-
+// express 
 app.get('/productos', async(req, res)=>{
     const pro = await producto.getAll()
     res.send(pro)
@@ -51,6 +65,25 @@ app.get('/productosRandom', async(req, res)=>{
     const random = parseInt(Math.random()* pro.length)
     res.send(pro[random])
 })
-const PORT = 3000;
 
+// Router 
+const productos = []
+router.get('/',(req, res)=>{
+    res.json(productos)
+})
+router.get('/:id',(req,res)=>{
+    const id = producto.getById()
+    res.json(id)
+})
+router.post('/',(req,res)=>{
+    res.json({})
+})
+router.put('/:id',(req, res)=>{
+    //
+})
+router.delete('/:id',(req, res)=>{
+    //
+})
+app.use('/productos',router);
+const PORT = 3000;
 app.listen(PORT, ()=> console.log(`http://localhost:${PORT}`));
